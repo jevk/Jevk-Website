e02464516609714814d9400a25fd50642855f652
Jevk <58623854+jevk@users.noreply.github.com>
2025-04-04 01:13:19 +0300

    Builder scripts

--- js/main.js
+++ js/main.js
@@ -1,4 +1,61 @@
-function hideHam() {
-    document.getElementById('borgor').classList.toggle('expand')
-    document.getElementById('navText').classList.toggle('hide-text')
+"use strict";
+
+const navItems = [
+    { name: "Home", link: "index.html" },
+    { name: "Maps", link: "maps.html" },
+    { name: "ScoreSaber", link: "https://scoresaber.com/u/76561198100136966?page=1&sort=top", target: "_blank" },
+    { name: "Scripts", link: "https://github.com/jevk/JevkMaps", target: "_blank" },
+    { name: "HeckLib", link: "https://github.com/Heck-Library/HeckLib", target: "_blank" },
+];
+
+function buildNavbar() {
+    const navbar = document.querySelector(".nav-div");
+    const ul = document.createElement("ul");
+
+    navItems.forEach(item => {
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+
+        a.href = item.link;
+        a.textContent = item.name;
+
+        if (item.target) {
+            a.target = item.target;
+        }
+
+        li.appendChild(a);
+        ul.appendChild(li);
+    });
+
+    navbar.appendChild(ul);
+}
+
+const footerItems = [
+    { link: "https://discord.gg/GgfNCWbUgY", classes: "fa-brands fa-discord size" },
+    { link: "https://www.twitch.tv/jevk", classes: "fa-brands fa-twitch size" },
+    { link: "https://www.youtube.com/@jevk", classes: "fa-brands fa-youtube size" },
+    { link: "https://www.github.com/jevk", classes: "fa-brands fa-github size" },
+    { link: "https://beatsaver.com/profile/4284636", classes: "fa-solid fa-square-caret-down size"},
+];
+
+function buildFooter() {
+    const footer = document.createElement("footer");
+    footer.appendChild(document.createElement("br"));
+    
+    footerItems.forEach(item => {
+        const a = document.createElement("a");
+        a.href = item.link;
+        a.target = "_blank";
+
+        const i = document.createElement("i");
+        i.className = item.classes;
+
+        a.appendChild(i);
+        footer.appendChild(a);
+    });
+
+    footer.appendChild(document.createElement("br"));
+    footer.appendChild(document.createElement("br"));
+
+    document.body.appendChild(footer);
 }
---
+++ js/mapbox.js
@@ -0,0 +1,53 @@
+function mapBox(name, img, video, script) {
+    const mapBox = document.createElement("div");
+    mapBox.className = "map-box";
+    mapBox.style.backgroundImage = `url(${img})`;
+
+    const h3 = document.createElement("h3");
+    h3.innerText = name;
+    mapBox.appendChild(h3);
+
+    const ul = document.createElement("ul");
+
+    for (let i = 0; i < 2; i++) {
+        const li = document.createElement("li");
+        const a = document.createElement("a");
+
+        a.href = i == 0 ? video : script;
+        a.target = "_blank";
+        a.innerText = i == 0 ? "Video" : "Script";
+
+        li.appendChild(a);
+        ul.appendChild(li);
+    }
+
+    mapBox.appendChild(document.createElement("br"));
+    mapBox.appendChild(ul);
+    
+    const mapBoxContainer = document.querySelector(".map-box-container");
+    mapBoxContainer.appendChild(mapBox);
+}
+
+const mapData = [{ 
+    name: "Conflict", img: "/images/ConflictToast.png",
+    video: "https://www.youtube.com/watch?v=NicNQA67NqE",
+    script: "https://github.com/jevk/JevkMaps/tree/main/Conflict"
+}, {
+    name: "GHOUL", img: "/images/ghoul.png",
+    video: "https://youtu.be/o0vC9ymtMAg?si=7Tv3Ca679e1LKonS&t=15",
+    script: "https://github.com/jevk/JevkMaps/tree/main/GHOUL"
+}, { 
+    name: "Armageddon", img: "/images/Armageddon.png",
+    video: "https://www.youtube.com/watch?v=yO7H7taf2Mk",
+    script: "https://github.com/jevk/JevkMaps/tree/main/Armageddon"
+}, {
+    name: "GHOST", img: "/images/ghost.png",
+    video: "https://www.youtube.com/watch?v=xh3Vijx4xXc",
+    script: "https://github.com/jevk/JevkMaps/tree/main/GHOST"
+}];
+
+function buildAllMaps() {
+    mapData.forEach(map => {
+        mapBox(map.name, map.img, map.video, map.script);
+    });
+}
